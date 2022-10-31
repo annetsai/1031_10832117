7f61cc39c840d6ecdc50bbba45bddb6a7172e281
annetsai <114378840+annetsai@users.noreply.github.com>
2022-10-31 13:04:24 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/新增資料夾.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1031_10832117.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
